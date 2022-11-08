d3b02714c3700f57a3dab36a60264ca46984d417
HugoLCSB <hugolcsb@gmail.com>
2022-11-08 20:01:09 +0000

    changed character flip, to work on mouse dir instead of movement dir

--- Assets/Player_Hugo/CharacterController2D.cs
+++ Assets/Player_Hugo/CharacterController2D.cs
@@ -8,6 +8,7 @@ public class CharacterController2D : MonoBehaviour
 	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
 	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
+	[SerializeField] private bool m_SwitchCharacterFlip = false;				// Whether to flip the charater acoording to movement direction or mouse position;
 	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
 	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
 	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
@@ -71,6 +72,30 @@ public class CharacterController2D : MonoBehaviour
 		else{
 			m_coyoteTimeCounter -= Time.deltaTime;
 		}
+
+		//HANDLING CHARACTER FLIP WHEN ON MOUSE POSITION MODE
+
+		if(m_SwitchCharacterFlip){	// Flip according to mouse position
+				//Get the Screen positions of the object
+				Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+				//Get the Screen position of the mouse
+				Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+				//adding velocity according to the direction from mouse to player (kockback on player)
+				Vector2 facingDirection = (new Vector2(positionOnScreen.x - mouseOnScreen.x,
+					positionOnScreen.y - mouseOnScreen.y).normalized);
+
+				if (facingDirection.x < 0 && !m_FacingRight)
+				{
+					// ... flip the player.
+					Flip();
+				}
+				else if (facingDirection.x > 0 && m_FacingRight)
+				{
+					// ... flip the player.
+					Flip();
+				}
+		}
 	}
 
 
@@ -123,17 +148,22 @@ public class CharacterController2D : MonoBehaviour
 			// And then smoothing it out and applying it to the character
 			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
-			// If the input is moving the player right and the player is facing left...
-			if (move > 0 && !m_FacingRight)
-			{
-				// ... flip the player.
-				Flip();
-			}
-			// Otherwise if the input is moving the player left and the player is facing right...
-			else if (move < 0 && m_FacingRight)
-			{
-				// ... flip the player.
-				Flip();
+
+			//HANDLING CHARACTER FLIP WHEN ON MOVEMENT DIR MODE
+
+			if(!m_SwitchCharacterFlip){	// flip according to movement direction
+				// If the input is moving the player right and the player is facing left...
+				if (move > 0 && !m_FacingRight)
+				{
+					// ... flip the player.
+					Flip();
+				}
+				// Otherwise if the input is moving the player left and the player is facing right...
+				else if (move < 0 && m_FacingRight)
+				{
+					// ... flip the player.
+					Flip();
+				}
 			}
 		}
 
